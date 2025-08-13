5ad88a880ddc665f9864222a9acbce3a6dc30fd6
Aastha Verma <aasthav3762@gmail.com>
2025-08-13 10:37:11 -0500

    signup page; basic fields needs submit logic

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,91 @@
+import { React, useState } from "react";
+
 export default function SignUp() {
-  return (
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(
+            firstName,
+            lastName,
+            email
+        );
+        // Add your form submission logic here
+    };
+    const handleReset = () => {
+        // Reset all state variables here
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+    };
+
+    return (
     <main>
-      <h1>Sign Up!</h1>
+        <h1>Sign Up!</h1>
+        <div className="Fields">
+            <h2>Input Fields</h2>
+            <fieldset>
+                <form action="#" method="get">
+                    <label for="firstname">
+                        First Name
+                    </label>
+                    <input 
+                        type="text"
+                        name="firstname"
+                        id="firstname"
+                        value={firstName}
+                        onChange={(e) =>
+                            setFirstName(e.target.value)
+                        }
+                        placeholder="Enter First Name"
+                        required
+                    />
+                    <label for="lastname">
+                        Last Name
+                    </label>
+                    <input 
+                        type="text"
+                        name="lastname"
+                        id="lastname"
+                        value={lastName}
+                        onChange={(e) =>
+                            setLastName(e.target.value)
+                        }
+                        placeholder="Enter Last Name"
+                        required
+                    />
+                    <label for="email">
+                        Enter Email
+                    </label>
+                    <input 
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) =>
+                            setEmail(e.target.value)
+                        }
+                        placeholder="Enter Email"
+                        required
+                    />
+                    <button
+                        type="reset"
+                        value="reset"
+                        onClick={() => handleReset()}
+                    >
+                        Reset
+                    </button>
+                    <button
+                        type="submit"
+                        value="Submit"
+                        onClick={(e) => handleSubmit(e)}
+                    >
+                        Submit
+                    </button>
+                </form>
+            </fieldset>
+        </div>
     </main>
-  );
+    );
 }
